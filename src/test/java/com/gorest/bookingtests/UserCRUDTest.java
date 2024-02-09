6098cee270147d4bc71ce8fafd498abef5023a42
Shraddhadhaduk <shraddhadhaduk96@gmail.com>
2024-02-09 00:54:57 +0000

    gorest serenity bdd 02

---
+++ src/test/java/com/gorest/bookingtests/UserCRUDTest.java
@@ -0,0 +1,19 @@
+package com.gorest.bookingtests;
+
+import com.gorest.testbase.TestBase;
+import net.serenitybdd.annotations.Title;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class UserCRUDTest extends TestBase {
+
+
+    @Title("Create a new booking")
+    @Test
+    public void test001(){
+
+    }
+
+}
